d61be2c6eec2c467848a72d9c1943b7f58159553
BelenPuyoo <69061925+BelenPuyoo@users.noreply.github.com>
2020-11-27 20:37:38 -0300

    Update ConsultorioVeterinario.c

--- ConsultorioVeterinario.c
+++ ConsultorioVeterinario.c
@@ -1,18 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h> 
-
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
 typedef char cadena[80];
 
 
 
+
 struct Fecha
-{
-	int dd;
-	int mm;
-	int aaaa;
-};
-
-
+	int dd,mm,aa;
 {
 
 
