71e1ccb6ccce631bc3858f9f4a4fb81bc328a937
Celia992 <66139006+celia992@users.noreply.github.com>
2020-12-01 09:58:49 -0300

    Update Recepcion.c
    Menú del asistente

--- Recepcion.c
+++ Recepcion.c
@@ -17,3 +17,21 @@ struct mascota
 {
 	
 }
+int Menu()
+{
+	int opcion = 0;
+	
+	printf("Modulo de recepcion");
+	printf("\n----\n");
+	
+	printf("\n1- Iniciar Sesión");
+	printf("\n2- Registrar Mascota");
+	printf("\n3- Registrar Turno");
+	printf("\n4- Listado de atenciones por veterinario y fecha");
+
+	printf("\n0- Cerrar la aplicacion ");
+	printf("\n\nOpcion: "); 
+	scanf("%d", &opcion);
+	
+	return opcion;
+}
