1ceb010cd76c92e0e1b74787b6add509bae364c5
BelenPuyoo <69061925+BelenPuyoo@users.noreply.github.com>
2020-12-02 01:22:42 -0300

    Update Administracion.c

--- Administracion.c
+++ Administracion.c
@@ -13,15 +13,16 @@ struct Fecha
 int Menu() 
 { 
 	int opc = 0; 
-	 
-	printf("Modulo Administración"); 
-	printf("\n----\n");
-        printf("\n 1 - Registrar Veterinario"); 
-	printf("\n 2 - Registrar Usuario Asistente"); 
-	printf("\n 3 - Atenciones por Veterinarios"); 
-	printf("\n 4 - Ranking de Veterinarios por atenciones"); 
-	printf("\n 0 - Cerrar la aplicacion "); 
-	printf("\n\n Ingrese una opcion: "); 
+	 printf("\n\t\t\t\t\t\**********************************************************");
+	printf("\n\t\t\t\t\t\t***************Modulo Administración**********************\n");
+	printf("\t\t\t\t\t\t\t**********************************************************\n");
+	printf("\n\t\t\t\t\t\t\t**\t 1 - Registrar Veterinario \t\t\t\\t\t\t\t\t\t\t\t**\t");
+	printf("\n\t\t\t\t\t\t\t**\t 2 - Registrar Usuario Asistente \t\t\t\t\t\t\t\t\t**\t");
+	printf("\n\t\t\t\t\t\t\t**\t 3 - Atenciones por Veterinarios\t\t\t\t\t\tt\t\t\t**\t");
+	printf("\n\t\t\t\t\t\t\t**\t 4 - Ranking de Veterinarios por Atenciones\t\t\t\t**\t"); 
+        printf("\n\t\t\t\t\t\t\t**\t 5 - Cerrar la Aplicacion  \t\t\t\t\t\t\t\t\t\t\t\t**\t");
+	printf("\t\t\t\t\t\t\t\t***********************************************************\n");
+	printf("\n\n\t\t\t\t\t\t\t**\t - Ingrese una opcion: ");
 	scanf("%d", &opc); 
 	 
 	return opc; 
