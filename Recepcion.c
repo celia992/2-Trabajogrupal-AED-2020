da883a22cdca32addb88e2b028386fb75e9625be
BelenPuyoo <69061925+BelenPuyoo@users.noreply.github.com>
2020-12-02 01:29:11 -0300

    Update Recepcion.c

--- Recepcion.c
+++ Recepcion.c
@@ -30,16 +30,19 @@ void RegistrarTurnos(FILE*turnos){
 	
 int Menu()
 {
-	int opcion = 0;
+	int opcion = 5;
 	system("color 5");
 	system("cls");
-	printf("Modulo de Recepcion");
-	printf("\n 1 - Iniciar Sesión");
-	printf("\n 2 - Registrar Mascota");
-	printf("\n 3 - Registrar Turno");
-	printf("\n 4 - Listado de Atenciones por Veterinario y Fecha");
-        printf("\n 0 - Cerrar la aplicacion ");
-	printf("\n\n Ingrese una opcion:: "); 
+	printf("\n\t\t\t\t\t\t\t*********************************************************");
+	printf("\n\t\t\t\t\t\t\t******************Modulo de Recepcion********************\n");
+	printf("\t\t\t\t\t\t\t**********************************************************\n");
+	printf("\n\t\t\t\t\t\t\t**\t 1 - Iniciar Sesion\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t**\t");
+	printf("\n\t\t\t\t\t\t\t**\t 2 - Registrar Mascota\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t**\t");
+	printf("\n\t\t\t\t\t\t\t**\t 3 - Registrar Turno \t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t**\t");
+	printf("\n\t\t\t\t\t\t\t**\t 4 - Listado de Atenciones por Veterinario y Fecha\t\t**\t");
+        printf("\n\t\t\t\t\t\t\t**\t 5 - Cerrar la aplicacion \t\t\t\t\t\t\t\t\t\t\t\t\t\t**\t");
+	printf("\t\t\t\t\t\t\t\t************************************************************\n");
+	printf("\n\n\t\t\t\t\t\t\t**\t - Ingrese una opcion: ");
 	scanf("%d", &opc);
 	
 	return opc;
