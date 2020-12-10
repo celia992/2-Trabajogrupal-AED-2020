ff7dccc37cf3ce1e6a9c9874438f85ec170e546a
Celia992 <66139006+celia992@users.noreply.github.com>
2020-12-10 16:31:14 -0300

    Create Clave.h

---
+++ Clave.h
@@ -0,0 +1,8 @@
+#include <stdio.h>    //puts, scanf, printf, y otras funciones
+
+#include <stdlib.h> // sytem (..) y otras funciones 
+
+#include <string.h> // strcpy, strlwr, stren, strcat, strcat y otras funciones 
+
+
+//para contraseña
