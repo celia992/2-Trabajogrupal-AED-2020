7dc31c4008f39b89abd6ab5391c4104729453f12
olgam007 <68977487+olgam007@users.noreply.github.com>
2020-12-11 13:35:50 -0300

    Update Clave.h

--- Clave.h
+++ Clave.h
@@ -42,3 +42,30 @@ bool validarPass(cadena pass){
 			 	else otros++;
 		if (numcons == 4) break;
 	}
+	
+		return may>=1 && 
+		   min>=1 && 
+		   num>=1 && 
+		   otros==0 && 
+	       strlen(pass)>=6 && 
+		   strlen(pass)<=32 && 
+		   numcons<=3 && 
+		   validarConsecutivasPass(pass);
+}
+
+bool validarConsecutivasPass(cadena pass){
+	int letrasCons = 1;
+	
+	strlwr(pass);
+	//aBuel123
+	for(int i=0; i<strlen(pass); i++){
+		if (pass[i] >='a' && pass[i] <='z'){
+			if(i>0 && pass[i-1]>'9' && pass[i]==pass[i-1]+1) letrasCons++;
+			//else letrasCons = 0;			
+		}
+		
+		if (letrasCons == 2) break;
+	}	
+	return letrasCons < 2;
+}
+	
